201b00d4816fbc0dc1a56cf67adc4a75b224b738
Lucas Raoni <lucasraoniha@gmail.com>
2025-08-04 19:31:12 -0300

    Feat: Adicionando route to direct the result of the analizer

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
-from flask import Flask, send_file
+from flask import Flask, jsonify, send_file
 import io
 import logging
 
+from main import make_analisis
+
 app = Flask(__name__)
 app.logger.setLevel(logging.DEBUG)
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
 @app.route("/get_image")
 def get_image():
-    with open("./image/graph.gv.jpg", 'rb') as f:
+    with open("./image/graph.gv.jpg", "rb") as f:
         image_data = f.read()
 
-    return send_file(io.BytesIO(image_data), mimetype='image/jpeg')
+    return send_file(io.BytesIO(image_data), mimetype="image/jpeg")
+
+
+@app.route("/get_analisis")
+def get_analisis():
+    result = make_analisis()
+
+    return jsonify(result)
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,65 +1,95 @@
 import pandas as pd
 import networkx as nx
-import matplotlib.pyplot as plt
-from graphviz import Source # is needed to install graphviz in the docker container
+from graphviz import Source
 import os
-   
+
 from collections import OrderedDict
 
+
 def plot_graph(G):
-   A = nx.nx_agraph.to_agraph(G)
+    A = nx.nx_agraph.to_agraph(G)
 
-   os.remove('./dot/graph.dot') if os.path.exists('./dot/graph.dot') else None
-   os.remove('./image/graph.gv') if os.path.exists('./image/graph.gv') else None
+    os.remove("./dot/graph.dot") if os.path.exists("./dot/graph.dot") else None
+    os.remove("./image/graph.gv") if os.path.exists("./image/graph.gv") else None
 
-   A.write('./dot/graph.dot')
-   s = Source.from_file('./dot/graph.dot')
-   s.render('./image/graph.gv', format='jpg', view=True)
+    A.write("./dot/graph.dot")
+    s = Source.from_file("./dot/graph.dot")
+    s.render("./image/graph.gv", format="jpg", view=True)
+
 
 def get_n_highest_values(data, n=2, order=False):
     top = sorted(data.items(), key=lambda x: x[1], reverse=True)[:n]
+
     if order:
         return OrderedDict(top)
     return dict(top)
 
+
+def make_analisis():
+    df = pd.read_csv("data.csv", comment="#", sep=", ", header=0, engine="python")
+
+    G = nx.DiGraph()
+    G.add_weighted_edges_from(
+        df[["id_sender", "id_reciever", "value"]]
+        .astype({"id_sender": str, "id_reciever": str, "value": float})
+        .values
+    )
+
+    # isso aqui eh os nodes que tem a maior quantidade de shortest paths
+    centrality = nx.betweenness_centrality(G)
+
+    # Measures the number of edges connected to a node. Nodes
+    # with higher degree centrality are well-connected
+    # olhar os valores mais altos, pois se algum desses falhar,
+    # problemas podem acontencer
+    degree_cent = nx.degree_centrality(G)
+
+    # Measures how close a node is to all other nodes, based on the shortest paths.
+    # Higher closeness centrality means the node can reach others more quickly
+    close_cent = nx.closeness_centrality(G)
+
+    # Find affected topics if a node is removed
+    # impact = len(nx.descendants(G, "data_structures"))
+
+    # Centralize all the results to send to the frontend
+    result = [
+        (
+            get_n_highest_values(centrality),
+            f"Esse caso de centralidade: {get_n_highest_values(centrality)}",
+        ),
+        (
+            get_n_highest_values(degree_cent),
+            f"Esse caso dos nodes conectados por muitos nodes centrais: {get_n_highest_values(degree_cent)}",
+        ),
+        (
+            get_n_highest_values(close_cent),
+            f"Esse caso de nodes que tem como menor caminho os nodes centrais: {get_n_highest_values(close_cent)}",
+        ),
+        (
+            G.size(weight="weight"),
+            f"Esse caso do valor total do mapa: {G.size(weight='weight')}",
+        ),
+        # (
+        #    impact,
+        #    f"Esse caso de impacto quando node removido: {impact}",
+        # ),
+    ]
+
+    return result
+
+
 def main():
-   if not os.path.isdir('./dot'):
-      os.makedirs('./dot')
+    if not os.path.isdir("./dot"):
+        os.makedirs("./dot")
 
-   if not os.path.isdir('./image'):
-      os.makedirs('./image')
+    if not os.path.isdir("./image"):
+        os.makedirs("./image")
 
-   # Load data
-   df = pd.read_csv('data.csv', comment='#', sep=', ', header=0, engine='python')
+    result = make_analisis()
 
-   G = nx.DiGraph()
-   G.add_weighted_edges_from(
-      df[['id_sender', 'id_reciever', 'value']]
-         .astype({'id_sender': str, 'id_reciever': str, 'value': float})
-         .values
-   )
+    print(result)
 
-   # isso aqui eh os nodes que tem a maior quantidade de shortest paths
-   centrality = nx.betweenness_centrality(G)
+    # plot_graph(G)
 
-   # Measures the number of edges connected to a node. Nodes with higher degree centrality are well-connected
-   # olhar os valores mais altos, pois se algum desses falhar, problemas podem acontencer
-   degree_cent = nx.degree_centrality(G)
-
-   # Measures how close a node is to all other nodes, based on the shortest paths. Higher closeness centrality means the node can reach others more quickly
-   close_cent = nx.closeness_centrality(G)
-
-   # Find affected topics if a node is removed
-   # impact = len(nx.descendants(G, 'data_structures'))
-
-   print(centrality)
-   print(get_n_highest_values(centrality))
-   print(degree_cent)
-   print(close_cent)
-   # all the weights from the edges
-   print(G.size(weight='weight'))
-   # print(impact)
-
-   # plot_graph(G)
 
 main()
