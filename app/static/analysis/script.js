53facd6523728ab199c83f9be77e2d0f1c2ead1f
Lucas Raoni <lucasraoniha@gmail.com>
2025-08-11 17:34:25 -0300

    Feat: Ajustando pagina de analysis

--- app/static/analysis/script.js
+++ app/static/analysis/script.js
@@ -1,3 +1,9 @@
+const element = (tag, props = {}) =>
+  Object.assign(document.createElement(tag), props);
+const append = (par, ...sibs) =>
+  sibs.reduce((p, sib) => (p.appendChild(sib), p), par);
+const queryEl = (qStr, el = document) => el.querySelector(qStr);
+
 const btn = document.getElementById("showBtn");
 const img = document.getElementById("analysisImage");
 const text = document.getElementById("analysisText");
@@ -14,6 +20,23 @@ $(document).ready(function () {
       type: "GET",
       success: function (_response) {
         $("#analysisImage").attr("src", "get_image");
+      },
+      error: function (_xhr) {},
+    });
+
+    $.ajax({
+      url: "get_analysis",
+      type: "GET",
+      success: function (response) {
+        response.forEach((item) => {
+          const el = element("p", {
+            id: "analysisText",
+            className: "analysis",
+          });
+          el.textContent = item[1];
+          el.style.display = "block";
+          append(queryEl("#analysisDivText"), el);
+        });
       },
       error: function (_xhr) {},
     });
